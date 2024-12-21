ba8a4ffd9b073d0549f6535b3160ee9043a0bf64
Wilfred Muchire <logincoin40@gmail.com>
2024-12-21 23:05:44 +0300

    adding adding some responsiveness to the desing

--- starter/src/Reader/Pages/Home/MostRead.jsx
+++ starter/src/Reader/Pages/Home/MostRead.jsx
@@ -2,12 +2,14 @@ import BlogCard from "./BlogCard"
 
 const MostRead = () => {
   return (
-    <div className="p-4 mt-4 border-2 border-black w-full ">
-      <div>
-        {" "}
-        <h1>Most Read</h1>
-        <button>View More</button>
+    <div className="p-4 mt-4 w-full bg-[#FBF8F2]  ">
+      <div className="flex justify-between items-center flex-row">
+        <h1 className="text-3xl font-san font-bold ">Most Read</h1>
+        <button className="bg-[#15D79F] text-black p-2 w-24 rounded-sm hover:scale-95 hover:transition-all ">
+          View More
+        </button>
       </div>
+      <hr className="h-1 bg-[#1B4942] mt-2" />
       <div>
         <BlogCard />
       </div>
