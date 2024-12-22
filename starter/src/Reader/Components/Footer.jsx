ef94f1fe70d3d4169921b34be633160004ffacc3
Wilfred Muchire <logincoin40@gmail.com>
2024-12-22 18:52:24 +0300

    making improvements to the footer..

--- starter/src/Reader/Components/Footer.jsx
+++ starter/src/Reader/Components/Footer.jsx
@@ -6,55 +6,55 @@ import { FaLinkedinIn } from "react-icons/fa";
 const Footer = () => {
   return (
     <div>
-      <div className="grid grid-cols-3 bg-[#222222] p-8 grid-rows-2 gap-2 h-max">
-        <div>
-          <h1 className="text-5xl font-bold  text-white"> Starter Blogs</h1>
-          <p className="pt-8 p-2     text-white tracking-wide">
-            Join over <span>1000 +</span> bloggers who use Starter Blog to share
-            and advance their knowledge in the business sector. Easily create
-            and share your knowledge, and guides via a blogs and explore more
-            tools that can move your business to the next level.{" "}
-          </p>
+      <div className="flex bg-[#222222] p-8 gap-2 h-max flex-col ">
+        <div className="flex flex-row gap-10">
+          <div className="w-1/2">
+            <h1 className="text-5xl font-bold  text-white"> Starter Blogs</h1>
+            <p className="pt-8 p-2     text-white tracking-wide">
+              Join over <span>1000 +</span> bloggers who use Starter Blog to
+              share and advance their knowledge in the business sector. Easily
+              create and share your knowledge, and guides via a blogs and
+              explore more tools that can move your business to the next level.{" "}
+            </p>
 
-          <div className="mt-8 flex flex-row gap-2">
-            <div className="border-2 border-white rounded-full grid place-content-center w-12 h-12 hover:text-red-600   hover:scale-110 transition delay-150 duration-300 ease-in-out">
-              <FaFacebookF className="text-white text-2xl" />
+            <div className="mt-8 flex flex-row gap-2">
+              <div className="border-2 border-white rounded-full grid place-content-center w-12 h-12 hover:text-red-600   hover:scale-110 transition delay-150 duration-300 ease-in-out">
+                <FaFacebookF className="text-white text-2xl" />
+              </div>
+              <div className="border-2 border-white rounded-full grid place-content-center w-12 h-12 hover:text-red-600   hover:scale-110 transition delay-150 duration-300 ease-in-out">
+                <FaGoogle className="text-white text-2xl" />
+              </div>{" "}
+              <div className="border-2 border-white rounded-full grid place-content-center w-12 h-12 hover:text-red-600   hover:scale-110 transition delay-150 duration-300 ease-in-out">
+                <FaLinkedinIn className="text-white text-2xl" />
+              </div>{" "}
             </div>
-            <div className="border-2 border-white rounded-full grid place-content-center w-12 h-12 hover:text-red-600   hover:scale-110 transition delay-150 duration-300 ease-in-out">
-              <FaGoogle className="text-white text-2xl" />
-            </div>{" "}
-            <div className="border-2 border-white rounded-full grid place-content-center w-12 h-12 hover:text-red-600   hover:scale-110 transition delay-150 duration-300 ease-in-out">
-              <FaLinkedinIn className="text-white text-2xl" />
-            </div>{" "}
+          </div>
+
+          <div className="w-1/2">
+            <h1 className="text-2xl text-white"> Explore</h1>
+            <div className="mt-6">
+              <Link className="text-lg text-white" to={"/start"}>
+                Start
+              </Link>
+            </div>
+            <div>
+              <Link className="text-lg text-white" to={"/grow"}>
+                Grow
+              </Link>
+            </div>
+            <div>
+              <Link className="text-lg text-white" to={"/monetize"}>
+                Monetize
+              </Link>
+            </div>
           </div>
         </div>
-        <div>
-          <h1 className="text-2xl text-white"> Explore</h1>
-          <div className="mt-6">
-            <Link className="text-lg text-white" to={"/start"}>
-              Start
-            </Link>
-          </div>
-          <div>
-            <Link className="text-lg text-white" to={"/grow"}>
-              Grow
-            </Link>
-          </div>
-          <div>
-            <Link className="text-lg text-white" to={"/monetize"}>
-              Monetize
-            </Link>
-          </div>
-        </div>
-        <div>
-          <h1 className="text-2xl text-white">Company</h1>
-          <div>Newsletter</div>
-        </div>
+
         <div className=" h-max mt-10 col-span-3 flex flex-row justify-between items-center">
           <h1 className="text-white">
             Copyright © 2024 Starter, All rights reserved.
           </h1>
-          <div className='flex flex-row gap-3 text-white '>
+          <div className="flex flex-row gap-3 text-white ">
             <h1>Privacy Policy</h1>
             <h1>Terms of Use</h1>
             <h1>Cookies Policy</h1>
--- starter/src/Reader/Components/SubscribePage.jsx
+++ starter/src/Reader/Components/SubscribePage.jsx
@@ -1,21 +1,13 @@
-import subscribeImg from '../../assets/subscribe.jpg'
 import SubscribeInput from './SubscribeInput';
 
 const SubscribePage = () => {
   return (
-    <div className="bg-[#FBF8F2] mb-8">
-      <div className="bg-[#1B4942] flex  items-center gap-10 ">
-        <div className="w-1/2 ">
-          <img
-            src={subscribeImg}
-            alt=" blog image"
-            className="w-full h-96 block object-cover"
-          />
-        </div>
-        <div className='flex flex-col items-center  gap-4'>
-          <h1 className='text-3xl text-white font-bold'>Subscribe to our news letter </h1>
-      <SubscribeInput />
-        </div>
+    <div className="bg-[#1B4942] h-52  grid place-content-center">
+      <div className="flex flex-row items-center  gap-4">
+        <h1 className="text-3xl text-white font-bold">
+          Subscribe to our news letter{" "}
+        </h1>
+        <SubscribeInput />
       </div>
     </div>
   );
