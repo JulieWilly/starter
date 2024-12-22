32461ed161153fc09228637e21a20dd0b2ff170d
Wilfred Muchire <logincoin40@gmail.com>
2024-12-22 13:29:53 +0300

    design for the lest updates section

--- starter/src/Reader/Pages/Home/Latest/LatestCard.jsx
+++ starter/src/Reader/Pages/Home/Latest/LatestCard.jsx
@@ -1,33 +1,84 @@
+import { Link } from "react-router";
+import LeftCard from "./LeftCard";
+import { MdNavigateNext } from "react-icons/md";
 const LatestCard = () => {
   return (
-    <div className="grid grid-cols-2 grid-rows-3 border-2 gap-2 border-black">
-      <div className="row-span-3  max-h">
-        <div className="w-full h-[20rem]">
-          <img
-            className="w-full h-full block object-cover"
-            src="https://images.pexels.com/photos/29237846/pexels-photo-29237846/free-photo-of-golden-ginkgo-leaves-in-toronto-s-fall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-        </div>
-        <div className="p-2">
-          <h1>Ideas to</h1>
-          <h1 className="line-clamp-2 text-3xl font-bold mt-2 leading-normal ">
-            This is a story that I would like to tell to everyone
-          </h1>
-          <p className="line-clamp-2 text-xm mt-2 font-light text-xl">
-            Don&#39;t let these artists scam their way into your hearts
-          </p>
-          <div className="flex flex-row gap-4 items-center mt-4">
-            {" "}
-            <span>By Author</span>
+    <div className="grid grid-cols-2 p-2  grid-rows-3 gap-2 justify-center   ">
+      <LeftCard />
+      <div className="flex flex-col gap-2 p-2">
+        <div className="flex flex-row justify-between">
+          <div className="w-1/2  p-2">
+            <h1 className="font-bold text-lg"> Must read blogs</h1>
+            <h1 className="mt-4 mb-8 text-2xl font-bold line-clamp-3">
+              7 Movies to watch for this summer, &apos;The boss baby; Back in
+              the crib &apos;
+            </h1>
 
-            <span className="italic">June 12</span>
+            <span className="italic  ">March 5</span>
+          </div>
+          <div className="w-1/2  h-60">
+            <img
+              className="object-cover block h-full w-full"
+              src="https://images.pexels.com/photos/29237846/pexels-photo-29237846/free-photo-of-golden-ginkgo-leaves-in-toronto-s-fall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
           </div>
         </div>
+
+        <hr className="h-[.1rem] bg-black " />
       </div>
-      <div className="bg-red-500">B</div>
-      <div className="bg-green-500">C</div>
-      <div className="bg-orange-500">D</div>
+
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row justify-between">
+          <div className="w-1/2  p-2">
+            <h1 className="font-bold text-lg"> Must read blogs</h1>
+            <h1 className="mt-4 mb-8 text-2xl font-bold line-clamp-3">
+              7 Movies to watch for this summer, &apos;The boss baby; Back in
+              the crib &apos;
+            </h1>
+
+            <span className="italic  ">March 5</span>
+          </div>
+          <div className="w-1/2  h-60">
+            <img
+              className="object-cover block h-full w-full"
+              src="https://images.pexels.com/photos/29237846/pexels-photo-29237846/free-photo-of-golden-ginkgo-leaves-in-toronto-s-fall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <hr className="h-[.1rem] bg-black " />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row justify-between">
+          <div className="w-1/2  p-2">
+            <h1 className="font-bold text-lg"> Must read blogs</h1>
+            <h1 className="mt-4 mb-8 text-2xl font-bold line-clamp-3">
+              7 Movies to watch for this summer, &apos;The boss baby; Back in
+              the crib &apos;
+            </h1>
+
+            <span className="italic  ">March 5</span>
+          </div>
+          <div className="w-1/2  h-60">
+            <img
+              className="object-cover block h-full w-full"
+              src="https://images.pexels.com/photos/29237846/pexels-photo-29237846/free-photo-of-golden-ginkgo-leaves-in-toronto-s-fall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <hr className="h-[.1rem] bg-black " />
+      </div>
+      <div className="col-span-2 bg-red-500 h-max"></div>
+      <div className="w-full flex col-span-2  justify-end items-center">
+        <Link to={""} className="border-b-2 border-black">
+          More Blogs{" "}
+        </Link>{" "}
+        <MdNavigateNext className="text-3xl" />
+      </div>
     </div>
   );
 }
---
+++ starter/src/Reader/Pages/Home/Latest/LeftCard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const LeftCard = () => {
+  return (
+    <div className="row-span-3 w-full max-h flex items-center flex-col">
+      <div className="w-10/12 h-[20rem]">
+        <img
+          className="w-full h-full block object-cover"
+          src="https://images.pexels.com/photos/29237846/pexels-photo-29237846/free-photo-of-golden-ginkgo-leaves-in-toronto-s-fall.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+      </div>
+      <div className="p-2 w-10/12">
+        <h1>Ideas to</h1>
+        <h1 className="line-clamp-2 text-3xl font-bold mt-2 leading-normal ">
+          This is a story that I would like to tell to everyone
+        </h1>
+        <p className="line-clamp-2 text-xm mt-2 font-light text-xl">
+          Don&#39;t let these artists scam their way into your hearts
+        </p>
+        <div className="flex flex-row gap-4 items-center mt-4">
+          {" "}
+          <span>By Author</span>
+          <span className="italic">June 12</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftCard
