f4ad26e4ccfda40e5c44786302418f010a09899f
Wilfred Muchire <logincoin40@gmail.com>
2024-12-23 11:00:03 +0300

    update on the subscribe section

--- starter/src/Reader/Components/SubscribeInput.jsx
+++ starter/src/Reader/Components/SubscribeInput.jsx
@@ -5,9 +5,9 @@ const SubscribeInput = () => {
       <input
         type="email"
         placeholder=" Enter your email address "
-        className="focus:border-none focus:outline-none border-none ps-2 h-10 w-72"
+        className="focus:border-none focus:outline-none border-none ps-2 h-10 w-full"
       />
-      <button className="bg-[#15D79F] text-xl w-28 text-black p-2 w-24 rounded-sm hover:scale-95 hover:transition-all ">
+      <button className="bg-[#15D79F] text-xl w-28 text-black p-2  rounded-sm hover:scale-95 hover:transition-all ">
         Subscribe
       </button>
     </div>
--- starter/src/Reader/Components/SubscribePage.jsx
+++ starter/src/Reader/Components/SubscribePage.jsx
@@ -1,17 +1,28 @@
+import Image from "./Image";
 import SubscribeInput from "./SubscribeInput";
 import { TfiEmail } from "react-icons/tfi";
 const SubscribePage = () => {
   return (
-    <div className="  flex items-center justify-center ">
-      <div className="  mb-8 rounded-2xl  p-8 flex flex-col items-center  gap-4 w-4/5 bg-[#1B4942]">
-<TfiEmail className="text-[4rem]  text-white" />
-        <h1 className="text-3xl text-white font-bold">Join our news letter</h1>
-        <p className="w-4/5  text-center tracking-wide font-light text-white pb-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
-          assumenda! Deleniti recusandae, nostrum est eos ex asperiores cumque
-          obcaecati blanditiis 
+    <div className="  flex p-8  border border-black bg-[#1B4942]">
+      <div className="w-1/2 flex flex-col p-2">
+        <span className="text-[4rem] tracking-normal font-bold font-[cursive] text-[#8D1A5F] ">
+          Sign up to
+        </span>
+        <span className="text-[4rem] tracking-normal font-bold font-[cursive] text-white ">
+          Starter Blogs newsletter
+        </span>
+
+        <p className="text-white mt-2 font-light tracking-wide">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+          itaque ipsum. Harum et dignissimos accusantium explicabo quae quasi
+          doloremque ipsam.
         </p>
-        <SubscribeInput />
+        <div className="mt-6">
+          <SubscribeInput />
+        </div>
+      </div>
+      <div className="w-1/2 h-[30rem] ml-2">
+        <Image path={'https://thestory.au/_nuxt/image/319d7a.webp'} />
       </div>
     </div>
   );
