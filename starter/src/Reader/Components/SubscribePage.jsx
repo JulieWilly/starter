4156f5ce20cd56a9834e609341ba1b85bbd52d6e
Wilfred Muchire <logincoin40@gmail.com>
2024-12-24 09:07:53 +0300

    adding relevant changes to the subscribe section

--- starter/src/Reader/Components/SubscribePage.jsx
+++ starter/src/Reader/Components/SubscribePage.jsx
@@ -3,26 +3,23 @@ import SubscribeInput from "./SubscribeInput";
 import { TfiEmail } from "react-icons/tfi";
 const SubscribePage = () => {
   return (
-    <div className="  flex p-8  border border-black bg-[#1B4942]">
-      <div className="w-1/2 flex flex-col p-2">
-        <span className="text-[4rem] tracking-normal font-bold font-[cursive] text-[#8D1A5F] ">
-          Sign up to
+    <div className="  flex p-4  bg-[#1B4942] items-center flex-col">
+      <div className="flex  items-center flex-col p-2  w-3/4">
+        <span className="text-[3rem] tracking-normal font-bold font-[cursive]text-center text-[#8D1A5F] ">
+          Subscribe to
         </span>
-        <span className="text-[4rem] tracking-normal font-bold font-[cursive] text-white ">
+        <span className="text-[3rem] tracking-normal font-bold font-[cursive] text-white ">
           Starter Blogs newsletter
         </span>
 
-        <p className="text-white mt-2 font-light tracking-wide">
+        <p className="text-white w-1/2 text-center  mt-2 font-light tracking-wide">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore,
           itaque ipsum. Harum et dignissimos accusantium explicabo quae quasi
           doloremque ipsam.
         </p>
-        <div className="mt-6">
+        <div className="w-1/2 mt-8">
           <SubscribeInput />
         </div>
-      </div>
-      <div className="w-1/2 h-[30rem] ml-2">
-        <Image path={'https://thestory.au/_nuxt/image/319d7a.webp'} />
       </div>
     </div>
   );
